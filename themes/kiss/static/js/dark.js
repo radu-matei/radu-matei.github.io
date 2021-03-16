5ce087f9bf7f1ddca417ce5cc691dc6f43c29aa1
Radu M <root@radu.sh>
2021-03-16 01:58:52 -0700

    Add WASI HTTP article and add reactive dark mode
    Signed-off-by: Radu M <root@radu.sh>

--- themes/kiss/static/js/dark.js
+++ themes/kiss/static/js/dark.js
@@ -1,21 +1,41 @@
 var toggle = document.getElementById("dark-mode-toggle");
 var darkTheme = document.getElementById("dark-mode-theme");
 
-// the default theme is light
-var savedTheme = localStorage.getItem("dark-mode-storage") || "light";
+var systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+  ? "dark"
+  : "light";
+
+// the default theme is the system theme, unless the user has
+// explicitly overriden it.
+var savedTheme = localStorage.getItem("dark-mode-storage") || systemTheme;
 setTheme(savedTheme);
 
+// set the appropriate theme when the user toggles the button
 toggle.addEventListener("click", () => {
   if (toggle.className === "far fa-moon") {
     setTheme("dark");
+    localStorage.setItem("dark-mode-storage", "dark");
   } else if (toggle.className === "far fa-sun") {
     setTheme("light");
+    localStorage.setItem("dark-mode-storage", "light");
   }
 });
 
-function setTheme(mode) {
-  localStorage.setItem("dark-mode-storage", mode);
+// Add an event listener when the browser theme changes.
+// The user defined theme does take precedence, so if the
+// changed the browser theme, do not persist the change
+// across refreshes.
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (event) => {
+    if (event.matches) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  });
 
+function setTheme(mode) {
   if (mode === "dark") {
     darkTheme.disabled = false;
     toggle.className = "far fa-sun";
